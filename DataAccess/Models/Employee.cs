9c8d5e1069a6c1b94b69ef71400bd0456ebc9212
Tyabin Ivan Alekseyevich <Ivan_Tyabin@mail.ru>
2024-01-19 11:33:30 +0700

    - Employee must have company now

--- DataAccess/Models/Employee.cs
+++ DataAccess/Models/Employee.cs
@@ -15,7 +15,11 @@ namespace DataAccess.Models
         [MaxLength(100), MinLength(1)] public string? Patronymic { get; set; }
         [MaxLength(100), MinLength(1)] public string? Email { get; set; }
 
+        // Foreign keys
+        [Required] public required int CompanyID { get; set; }
+
         // Navigation
+        public virtual Company? Company { get; set; }
         public virtual ICollection<Project> ParticipatedProjects { get; set; } = new HashSet<Project>();
         public virtual ICollection<Project> ManagedProjects { get; set; } = new HashSet<Project>();
     }
