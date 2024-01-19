f314dd358f3ba5c058e79e30b3cfab926e796bbc
Tyabin Ivan Alekseyevich <Ivan_Tyabin@mail.ru>
2024-01-19 15:33:41 +0700

    - Added "NotEqual" annotation

---
+++ DataAccess/CustomAttributes/NotEqualAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess
+{
+    [NotMapped]
+    public class NotEqualAttribute(string otherProperty) : ValidationAttribute
+    {
+        private readonly string otherProperty = otherProperty;
+
+        protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+        {
+            var otherPropertyValue = validationContext.ObjectType.GetProperty(otherProperty)?.GetValue(validationContext.ObjectInstance, null);
+
+            if (value != null && value.Equals(otherPropertyValue))
+            {
+                return new ValidationResult(ErrorMessage ?? $"{validationContext.DisplayName} must not be equal to {otherProperty}.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
